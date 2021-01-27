bb1fd38f8977be52f04361d83bf4000e063a486b
faizy <ifaizymohd@gmail.com>
2021-01-27 23:27:20 +0530

    update

--- blog_main.py
+++ blog_main.py
@@ -1,9 +1,28 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+import json
 from datetime import datetime
 
+
+with open('config.json', 'r') as c:
+    params = json.load(c)["params"]
+
+local_server = True
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/thecodingchunks'
+app.config.update(
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = '465',
+    MAIL_USE_SSL = True,
+    MAIL_USERNAME = params['gmail-user'],
+    MAIL_PASSWORD=  params['gmail-password']
+)
+mail = Mail(app)
+if(local_server):
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+
 db = SQLAlchemy(app)
 
 
@@ -22,15 +41,15 @@ class Contacts(db.Model):
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', params=params)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', params=params)
 
 @app.route("/post")
 def post():
-    return render_template('post.html')
+    return render_template('post.html', params=params)
 
 @app.route("/contact", methods=['GET', 'POST'])
 def contact():
@@ -49,7 +68,7 @@ def contact():
 
         db.session.add(entry)
         db.session.commit()
-    return render_template('contact.html')
+    return render_template('contact.html', params=params)
 
 
 app.run(debug=True)
